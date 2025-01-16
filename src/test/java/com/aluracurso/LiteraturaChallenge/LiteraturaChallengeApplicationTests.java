5fce91aafb07f3cd184940989920480bb1c92537
valadezjessica <jessica.jessicavaladezv@gmail.com>
2025-01-15 22:35:29 -0600

    Se crea el proeycto con spring initializr

---
+++ src/test/java/com/aluracurso/LiteraturaChallenge/LiteraturaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracurso.LiteraturaChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
